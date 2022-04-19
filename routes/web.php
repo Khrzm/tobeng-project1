98f2d964b4eec86b2817491e7e62fa6a27d61346
Khrzm <khrzm@tuta.io>
2022-04-19 15:56:36 +0700

    Penambahan HomeController

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::group(['middleware' => ['web']], function () {
-    Route::GET('/', 'FE\LoginController@index');
-    Route::GET('login', 'FE\LoginController@login');
+    /*(Route::GET('/', 'FE\DashboardController@dashboardEcommerce')->name('dashboard-ecommerce');
 
+    */
+
+    Route::GET('/', 'FE\HomeController@index');
+    Route::GET('/home', 'FE\HomeController@index');
+    Route::GET('/login', 'FE\LoginController@login');
     Route::get('/nde/inbox', 'FE\InboxController@index');
+    Route::get('/nde/dashboard-ecommerce', 'FE\DashboardController@dashboardEcommerce');
     Route::get('/nde/role', 'FE\RoleController@index')->middleware('role:admin');
 });
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,6 @@
 const mix = require('laravel-mix');
+const exec = require('child_process').exec
+require('dotenv').config()
 
 /*
  |--------------------------------------------------------------------------
@@ -11,7 +13,49 @@ const mix = require('laravel-mix');
  |
  */
 
+ const glob = require('glob')
+ const path = require('path')
+ 
+/*
+ |--------------------------------------------------------------------------
+ | Vendor assets
+ |--------------------------------------------------------------------------
+ */
+
+function mixAssetsDir(query, cb) {
+    ;(glob.sync('resources/' + query) || []).forEach(f => {
+      f = f.replace(/[\\\/]+/g, '/')
+      cb(f, f.replace('resources', 'public'))
+    })
+}
+
 mix.js('resources/js/app.js', 'public/js')
     .postCss('resources/css/app.css', 'public/css', [
         //
     ]);
+
+
+
+mixAssetsDir('vendors/js/**/*.js', (src, dest) => mix.scripts(src, dest))
+mixAssetsDir('vendors/css/**/*.css', (src, dest) => mix.copy(src, dest))
+mixAssetsDir('vendors/**/**/images', (src, dest) => mix.copy(src, dest))
+mixAssetsDir('vendors/css/editors/quill/fonts/', (src, dest) => mix.copy(src, dest))
+mixAssetsDir('fonts', (src, dest) => mix.copy(src, dest))
+mixAssetsDir('fonts/**/**/*.css', (src, dest) => mix.copy(src, dest))
+mix.copyDirectory('resources/images', 'public/images')
+mix.copyDirectory('resources/data', 'public/data')
+
+mix
+  .js('resources/js/core/app-menu.js', 'public/js/core')
+  .js('resources/js/core/app.js', 'public/js/core')
+
+mix.then(() => {
+    if (process.env.MIX_CONTENT_DIRECTION === 'rtl') {
+      let command = `node ${path.resolve('node_modules/rtlcss/bin/rtlcss.js')} -d -e ".css" ./public/css/ ./public/css/`
+      exec(command, function(err, stdout, stderr) {
+        if (err !== null) {
+          console.log(err)
+        }
+      })
+    }
+})
